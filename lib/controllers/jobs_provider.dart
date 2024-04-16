4c6fba0c2b1016ab18394760fb0759e41bb04d97
lufuz <luxangelax@gmail.com>
2024-04-16 00:51:53 -0600

    Cambios en las dependencias

--- lib/controllers/jobs_provider.dart
+++ lib/controllers/jobs_provider.dart
@@ -3,9 +3,9 @@ import 'package:get/get.dart';
 import 'package:jobhubv2_0/constants/app_constants.dart';
 import 'package:jobhubv2_0/models/request/bookmarks/bookmarks_model.dart';
 import 'package:jobhubv2_0/models/response/bookmarks/all_bookmarks.dart';
-import 'package:jobhubv2_0/models/response/jobs/get_job.dart';
-import 'package:jobhubv2_0/models/response/jobs/jobs_response.dart';
+import 'package:jobhubv2_0/models/response/vacants/get_vacant.dart';
+import 'package:jobhubv2_0/models/response/vacants/vacants_response.dart';
 import 'package:jobhubv2_0/services/helpers/book_helper.dart';
-import 'package:jobhubv2_0/services/helpers/jobs_helper.dart';
+import 'package:jobhubv2_0/services/helpers/vacants_helper.dart';
 
 class JobsNotifier extends ChangeNotifier {}
